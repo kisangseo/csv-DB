ffa4fc148ff73c7fc169abb4ffb98f3bf604122b
Seo <KiSang.Seo@baltimorecity.gov>
2025-12-26 11:09:52 -0500

    updated index to show date

--- run_ingest_warrants.py
+++ run_ingest_warrants.py
@@ -1,5 +1,10 @@
 from ingest import ingest_warrants_csv, ingest_all_odyssey_civil_blobs
 
 if __name__ == "__main__":
+    print("=== STARTING WARRANTS INGEST ===")
     ingest_warrants_csv()
+
+    print("=== STARTING ODYSSEY CIVIL INGEST ===")
     ingest_all_odyssey_civil_blobs()
+
+    print("=== INGEST COMPLETE ===")
